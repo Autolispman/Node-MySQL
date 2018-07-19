36641b077abdba618a170a91e092cba04fce409a
Dan Hopper <dhopper2@sbcglobal.net>
2018-07-19 12:57:12 -0500

    Finshed 2 options for challenge 2

---
+++ assets/javascript/bamazonManager.js
@@ -0,0 +1,73 @@
+let inquirer = require("inquirer")
+let mysql = require("mysql")
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Socks101",
+    database: "bamazon"
+})
+
+let cart = {
+    items: []
+}
+
+function start() {
+    inquirer.prompt([
+        {
+            type: "rawlist",
+            name: "managerChoice",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
+        }
+    ]).then(function (answer) {
+        switch (answer.managerChoice) {
+            case "View Products for Sale":
+                connection.query("SELECT item_id, product_name, price, stock_quantity FROM products",
+                    [],
+                    function (err, res) {
+                        if (err) {
+                            console.log(err)
+                        }
+                        else {
+                            console.log(res)
+                            displaySaleItems(res)
+                            start()
+                        }
+                    })
+
+                break
+            case "View Low Inventory":
+                connection.query("SELECT item_id, product_name, price, stock_quantity FROM products WHERE stock_quantity < 6",
+                    [],
+                    function (err, res) {
+                        if (err) {
+                            console.log(err)
+                        }
+                        else {
+                            console.log(res)
+                            displaySaleItems(res)
+                            start()
+                        }
+                    })
+                break
+            case "Exit":
+                endSession()
+                break
+        }
+    })
+}
+
+function displaySaleItems(res) {
+    console.log("--------------------------------------")
+    for (let i = 0; i < res.length; i++) {
+        console.log(`${res[i].item_id} ${res[i].product_name} | $${res[i].price} | ${res[i].stock_quantity}`)
+    }
+    console.log("--------------------------------------")
+}
+
+function endSession() {
+    connection.end()
+}
+
+start()
+
